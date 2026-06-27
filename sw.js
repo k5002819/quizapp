ad91a3b731c5d7f6c7b78ff2d7688eaa4e8018b3
k5002819 <k5002819@gmail.com>
2026-06-27 15:27:10 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='quizapp-v3';const ASSETS=['./','index.html','style.css','app.js','questions.json','manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
