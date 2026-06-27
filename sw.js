6e1d497b5746de091c95475d246ef33880fcc375
k5002819 <k5002819@gmail.com>
2026-06-27 16:41:54 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='quizapp-v5-fixed';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))});self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
